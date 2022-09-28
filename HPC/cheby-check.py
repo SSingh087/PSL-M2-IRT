9c8dcc2c56966d48ea14c9afa06817a7fe3682ac
Shashwat Singh <66710757+SSingh087@users.noreply.github.com>
2022-09-28 14:12:41 +0200

    Update cheby-check.py

--- HPC/cheby-check.py
+++ HPC/cheby-check.py
@@ -1,17 +1,18 @@
 import cheby as c
 import scipy.integrate as integrate
 
-def cheby_matrix(N, M): 
-    print(c.Cheby(N, M).eval_matrix())
+def cheby_matrix(N): 
+    print(c.Cheby(N).eval_matrix())
     
-def test_orthogonality(N, M, x, n, m):
-    res, err = integrate.quad(c.Cheby(N, M).orth_func, -1, 1, args=(n, m))
-    print(res, err)
+def test_orthogonality(N, n, m):
+    res, err = integrate.quad(c.Cheby(N).orth_func, -1, 1, args=(n, m))
+    print(res)
     
-def eval_coeffs(N, M, n, x):
-    print(c.Cheby(N, M).eval_coeffs_pn(x, n))
-    print(c.Cheby(N, M).eval_coeffs_p(x))
-    
-#eval_coeffs(8, 8, 5, 1)
-cheby_matrix(8, 10)
-eval_coeffs(8, 10, 5, 2)
+def eval_coeffs(N, n, x):
+    print(c.Cheby(N).eval_coeffs_pn(x, n))
+    print(c.Cheby(N).eval_coeffs_p(x))
+
+cheby_matrix(8)
+eval_coeffs(8, 5, 2)
+test_orthogonality(8, 1, 2)
+test_orthogonality(8, 2, 2)
